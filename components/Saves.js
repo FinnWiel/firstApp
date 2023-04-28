b572c5b9755f8a53510213a79d5a5910d47ddc99
Finn2190 <finnvandewiel01@gmail.com>
2023-04-28 21:09:57 +0200

    margins

--- components/Saves.js
+++ components/Saves.js
@@ -4,7 +4,7 @@ import { Text, View, TextInput, StyleSheet } from "react-native";
 export default function Saves(props) {
     return (
         <View>
-            <View style={{height: 100, aspectRatio: 1, backgroundColor: '#005683', borderRadius: 10, flex: 0, alignItems: 'center', justifyContent: 'flex-end'}}>
+            <View style={{height: 100, aspectRatio: 1, backgroundColor: '#005683', borderRadius: 10, flex: 0, alignItems: 'center', justifyContent: 'flex-end', marginTop: 10}}>
                 <Text style={{color: '#fff', fontSize: 12, marginBottom: 2}}>{props.stat} Save</Text>
                 <View style={{height: "80%", width: '100%', backgroundColor: 'lightgray', borderRadius: 10, flex: 0, justifyContent: 'center', alignItems: 'center'}}>
                     <TextInput style={{fontSize: 32,}}>10</TextInput>
